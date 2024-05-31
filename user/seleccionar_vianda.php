6a5998eea7f43942d97456d89b551fd4584f966b
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-05-31 19:14:50 -0300

    main

--- user/seleccionar_vianda.php
+++ user/seleccionar_vianda.php
@@ -5,6 +5,9 @@ if (!isset($_SESSION['usuario']) || $_SESSION['rol'] != 'Usuario') {
     exit;
 }
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include '../includes/db_connect.php';
 
 // Obtener los hijos del usuario
