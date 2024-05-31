deb9b54255dee1e16508fdd45356e4a5ff189574
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-05-31 20:55:26 -0300

    main

--- user/procesar_pedido.php
+++ user/procesar_pedido.php
@@ -9,18 +9,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Obtener los datos del formulario
     $usuario_id = $_SESSION['usuario_id'];
     $hijo_id = $_POST['hijo_id'];
-    $id = $_POST['id'];
+    $vianda_id = $_POST['vianda_id'];
     $fecha = date('Y-m-d'); // Obtener la fecha actual
     $notas = $_POST['notas']; // Obtener las notas del pedido
 
     // Verificar si el ID de la vianda existe en la tabla menu
-    $query_verificar = "SELECT id FROM menu WHERE id = '$id'";
+    $query_verificar = "SELECT id FROM menu WHERE id = '$vianda_id'";
     $result_verificar = mysqli_query($conn, $query_verificar);
 
     if (mysqli_num_rows($result_verificar) > 0) {
         // Insertar el pedido en la base de datos, incluyendo las notas
-        $query = "INSERT INTO pedidos (usuario_id, hijo_id, id, fecha, notas) 
-                  VALUES ('$usuario_id', '$hijo_id', '$id', '$fecha', '$notas')";
+        $query = "INSERT INTO pedidos (usuario_id, hijo_id, vianda_id, fecha, notas) 
+                  VALUES ('$usuario_id', '$hijo_id', '$vianda_id', '$fecha', '$notas')";
         if (mysqli_query($conn, $query)) {
             $mensaje = "Pedido realizado con éxito.";
         } else {
@@ -34,3 +34,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 echo $mensaje;
+
