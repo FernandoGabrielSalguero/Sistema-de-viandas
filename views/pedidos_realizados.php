1f6b7f03d1b39f1e362c0132a56f091df638afd1
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-06-03 19:43:43 -0300

    main

--- views/pedidos_realizados.php
+++ views/pedidos_realizados.php
@@ -45,7 +45,9 @@ $saldo = $saldo_result->num_rows > 0 ? $saldo_result->fetch_assoc()['saldo'] : 0
     <div class="header">
         <h1>Estos son tus pedidos, <?php echo htmlspecialchars($userInfo['nombre']); ?>!</h1>
         <p>Saldo: $<?php echo number_format($saldo, 2); ?></p>
+        <button onclick="location.href='user_dashboard.php'">Inicio</button>
         <button onclick="location.href='../php/logout.php'">Cerrar sesión</button>
+
         <button onclick="window.open('https://wa.me/542613406173', '_blank')">Contacto</button>
     </div>
     <table>
