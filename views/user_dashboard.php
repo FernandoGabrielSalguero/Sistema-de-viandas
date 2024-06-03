f982eb8a195560e91d25727209987d9d48bfec8b
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-06-03 18:22:22 -0300

    main

--- views/user_dashboard.php
+++ views/user_dashboard.php
@@ -166,7 +166,7 @@ if ($menus_result->num_rows > 0) {
             </tbody>
         </table>
     </div>
-    <?php include 'details_card.php'; ?>
+    <?php include 'pedidos_realizados.php'; ?>
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
