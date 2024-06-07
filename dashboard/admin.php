3b5e24d24520f1d15db91cbab3bdad95da0685fa
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-06-07 14:27:43 -0300

    main

--- dashboard/admin.php
+++ dashboard/admin.php
@@ -1,13 +1,1 @@
 <?php include '../headers/header_admin.php'; ?>
-
-<h1>Dashboard del Administrador</h1>
-<div>
-    <h2>Control de Usuarios</h2>
-    <button onclick="location.href='manage_users.php'">Gestionar Usuarios</button>
-    <h2>Control de Menús</h2>
-    <button onclick="location.href='manage_menus.php'">Gestionar Menús</button>
-    <h2>Vista de Pedidos</h2>
-    <button onclick="location.href='view_orders.php'">Ver Pedidos</button>
-    <h2>Reportes Financieros</h2>
-    <button onclick="location.href='financial_reports.php'">Ver Reportes Financieros</button>
-</div>
