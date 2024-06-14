0f69f520185006770f7cb6aaf77c87b9b2e68a35
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-06-14 14:12:02 -0300

    main

--- login.php
+++ login.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>¡Que bueno verte de nuevo!</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="login-container">
         <form action="login_process.php" method="post">
-            <h2>Login</h2>
-            <label for="username">Username:</label>
+            <h2>Ingresa</h2>
+            <label for="username">Usuario:</label>
             <input type="text" id="username" name="username" required>
-            <label for="password">Password:</label>
+            <label for="password">Contraseña:</label>
             <input type="password" id="password" name="password" required>
-            <button type="submit">Login</button>
+            <button type="submit">Ingresar</button>
         </form>
     </div>
 </body>
