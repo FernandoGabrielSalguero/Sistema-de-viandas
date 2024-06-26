0f766ee1183cef1f8de77135b8d20f5c01016f35
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-06-26 11:33:38 -0300

    main

--- common/db_connect.php
+++ common/db_connect.php
@@ -4,6 +4,11 @@ $dbname = 'u437094107_viandas_sch00l';
 $username = 'u437094107_adm111n';
 $password = '9t:RuQ7^nr+/';
 
+// Habilitar errores (solo en desarrollo)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
