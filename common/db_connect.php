ad15a335373f362c22e2d5f223c3f477a4b52e03
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-06-26 11:40:39 -0300

    main

--- common/db_connect.php
+++ common/db_connect.php
@@ -4,20 +4,16 @@ $dbname = 'u437094107_viandas_sch00l';
 $username = 'u437094107_adm111n';
 $password = '9t:RuQ7^nr+/';
 
-// Habilitar errores (solo en desarrollo)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Intentar conexión a la base de datos
 $conn = new mysqli($host, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     $error_message = "Conexión fallida: " . $conn->connect_error;
     echo "<script>console.error('".$error_message."');</script>";
     die($error_message);
 }
 
-// Confirmar conexión exitosa
 echo "<script>console.log('Conexión exitosa a la base de datos');</script>";
