c46864b7ae1701fb8088e47537af4673c3db02a6
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-07-08 12:00:52 -0300

    main

--- includes/header_papas.php
+++ includes/header_papas.php
@@ -1,3 +1,19 @@
+<?php
+// Habilitar la muestra de errores
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+include 'db.php';
+include 'functions.php';
+
+if (!isset($_SESSION['user_id']) || getUserRole($_SESSION['user_id']) !== 'papas') {
+    header("Location: ../login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
