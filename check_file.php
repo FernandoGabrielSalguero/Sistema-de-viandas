b5d7dbba6c1db655e394c99dbe070bfa5e08cc16
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-07-09 14:19:42 -0300

    main

--- check_file.php
+++ check_file.php
@@ -1,5 +1,5 @@
 <?php
-if (file_exists('admin/dashboard.php')) {
+if (file_exists('cocina/dashboard.php')) {
     echo 'El archivo admin/dashboard.php existe.';
 } else {
     echo 'El archivo admin/dashboard.php no existe.';
