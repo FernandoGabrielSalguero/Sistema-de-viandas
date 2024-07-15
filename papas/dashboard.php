707e8da972febd2fe5cdffc772cb53431fc722d8
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-07-15 11:08:23 -0300

    main

--- papas/dashboard.php
+++ papas/dashboard.php
@@ -78,6 +78,8 @@ $menus = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <title>Dashboard Papás</title>
     <link rel="stylesheet" href="../css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <style>
         .filters {
             display: flex;
