9b386675047b0bafe0fa0de7196a78d1e407c17c
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-07-15 11:03:36 -0300

    main

--- includes/header_papas.php
+++ includes/header_papas.php
@@ -4,6 +4,33 @@
     <meta charset="UTF-8">
     <title>Panel de Papás</title>
     <link rel="stylesheet" href="../css/styles.css">
+    <style>
+        nav {
+            background-color: red;
+            padding: 10px;
+        }
+        nav ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            display: flex;
+            justify-content: space-around;
+        }
+        nav ul li {
+            margin: 0 10px;
+        }
+        nav ul li a {
+            color: white;
+            background-color: green;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            font-weight: bold;
+        }
+        nav ul li a:hover {
+            background-color: darkgreen;
+        }
+    </style>
 </head>
 <body>
     <nav>
@@ -14,3 +41,5 @@
             <li><a href="../papas/logout.php">Salir</a></li>
         </ul>
     </nav>
+</body>
+</html>
