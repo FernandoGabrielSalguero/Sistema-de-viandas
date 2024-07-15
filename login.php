c7737e9af8c0c6b517cd4889b6651896c699ec69
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-07-15 11:10:45 -0300

    main

--- login.php
+++ login.php
@@ -56,6 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>¡Que gusto verde de nuevo!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
