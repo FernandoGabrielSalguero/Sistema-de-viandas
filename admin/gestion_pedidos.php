95828c0cffd7cabeb3c6833d19e3ce9cfafc2b4d
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-07-22 15:39:46 -0300

    main

--- admin/gestion_pedidos.php
+++ admin/gestion_pedidos.php
@@ -81,7 +81,7 @@ $kpi_data = $kpi_stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title>Gestión de Pedidos</title>
-    <link rel="stylesheet" href="../css/style_admin.css">
+    <link rel="stylesheet" href="../css/syle_admin.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
