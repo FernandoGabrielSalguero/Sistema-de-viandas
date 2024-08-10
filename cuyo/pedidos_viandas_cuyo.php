4f1499d2115680454ae811c14942b05a6b74419b
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-08-10 20:40:43 -0300

    main

---
+++ cuyo/pedidos_viandas_cuyo.php
@@ -0,0 +1,29 @@
+<?php
+// Habilitar la muestra de errores
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+include '../includes/header_cuyo_placa.php';
+include '../includes/db.php';
+
+// Verificar si el usuario está autenticado y tiene el rol correcto
+if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] != 'cuyo_placa') {
+    header("Location: ../index.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hola mundo</p>
+</body>
+</html>
