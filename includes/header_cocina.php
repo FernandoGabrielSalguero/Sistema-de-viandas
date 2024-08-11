4288af5104b3cefdd8bd61a5713c23fd3dc000a8
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-08-10 21:33:58 -0300

    main

--- includes/header_cocina.php
+++ includes/header_cocina.php
@@ -16,9 +16,21 @@ if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'cocina') {
     exit();
 }
 ?>
-<nav>
-    <ul>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
         <!-- <li><a href="pedidos.php">Pedidos</a></li> -->
         <li><a href="logout.php">Salir</a></li>
     </ul>
 </nav>
+</body>
+</html>
+<nav>
+
