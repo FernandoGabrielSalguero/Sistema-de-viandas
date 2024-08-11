7b24fd94e4e623bd3389a3a29625c74453e831bd
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-08-10 21:50:20 -0300

    main

--- includes/header_cocina.php
+++ includes/header_cocina.php
@@ -33,7 +33,7 @@ if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'cocina') {
             flex-wrap: wrap;
             gap: 10px;
             justify-content: center;
-            background-color: #f5f5f5;
+            background-color: #28a745;
             padding: 20px;
         }
 
