370c32afaa8c53ecb1e635a5d41298d497f78a17
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-08-15 10:20:24 -0300

    main

--- admin/gestion_pedidos_cuyo.php
+++ admin/gestion_pedidos_cuyo.php
@@ -1,14 +1,10 @@
 <?php
-session_start();
-include '../includes/header_admin.php'; // Asegúrate de que este archivo header sea específico para el rol de administrador
-include '../includes/db.php';
-date_default_timezone_set('America/Argentina/Buenos_Aires');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Verificar si el usuario está autenticado y tiene el rol correcto
-if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] != 'admin') {
-    header("Location: ../index.php");
-    exit();
-}
+include '../includes/header_admin.php';
+include '../includes/db.php';
 
 $fecha_inicio = '';
 $fecha_fin = '';
