9f284518e1347de91916ee87ad307233ffe8a0e9
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-08-28 08:52:41 -0300

    main

--- representante/dashboard.php
+++ representante/dashboard.php
@@ -68,7 +68,6 @@ $pedidos_viandas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title>Dashboard Representante</title>
-    <link rel="stylesheet" href="../css/styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         .filters {
