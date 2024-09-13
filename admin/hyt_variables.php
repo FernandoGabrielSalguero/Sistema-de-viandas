96584a19cd4f5dae93918064cd47653e119441c2
FernandoGabrielSalguero <fernandosalguero685@gmail.com>
2024-09-13 13:55:35 -0300

    main

--- admin/hyt_variables.php
+++ admin/hyt_variables.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include '../includes/header_admin.php';
-include '..includes/db.php';
+include '../includes/db.php';
 
 
 // Gestión de precios_hyt
